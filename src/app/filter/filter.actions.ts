f3c7b79ca3c9c1e58dba69e808c846dab38272e4
Sawako <saturnoxspace@outlook.com>
2020-03-27 16:22:53 -0600

    [PARTE 9]: Verificando el estado del filtro en los botones y cambiando el string del store filtro.

--- src/app/filter/filter.actions.ts
+++ src/app/filter/filter.actions.ts
@@ -1,7 +1,7 @@
 import { Action } from '@ngrx/store';
 
 export const SET_FILTER = '[FILTER] Establecer filtrador.'
-export type filtersValids = 'todos' | 'completados' | 'pendientes';
+export type filtersValids = 'todos' | 'terminados' | 'activos';
 
 export class SetFiltroAction implements Action {
     type = SET_FILTER;
--- src/app/todo/todo-footer/todo-footer.component.ts
+++ src/app/todo/todo-footer/todo-footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AppState } from 'src/app/app.reducers';
+import { Store } from '@ngrx/store';
+import { filtersValids, SetFiltroAction } from "../../filter/filter.actions";
 
 @Component({
   selector: 'app-todo-footer',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoFooterComponent implements OnInit {
 
-  constructor() { }
+  public filtrosValidos: filtersValids[] = ['terminados', 'activos', 'todos'];
+  public filtroActual: string;
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    this.store.subscribe(state => {
+      this.filtroActual = state.filtro;
+    })
   }
 
+  public cambiarFiltro(nuevoFiltro: filtersValids) {
+    const actionCambiarFiltro = new SetFiltroAction(nuevoFiltro);
+    this.store.dispatch(actionCambiarFiltro);
+  }
 }
